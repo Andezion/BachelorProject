26675d14cd7e18539c64408b7137ff2a44d7215a
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-02 11:18:05 +0200

    Delete build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Test1_FILE C:/Qt/Projects/Test1/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/Test1.exe)
-set(__QT_DEPLOY_TARGET_Test1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Test1_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
